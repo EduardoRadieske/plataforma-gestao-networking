40c670cf9bedb214c0c6dfebac052e221f15938c
Eduardo Radieske <eduardoradieske@gmail.com>
2025-11-11 10:52:36 -0300

    Melhorias layout HomePage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -8,12 +8,26 @@ export default function HomePage() {
         <p className="text-gray-600 mb-6">
           Um ambiente colaborativo para membros e empreendedores conectarem oportunidades.
         </p>
-        <Link
-          href="/intencao"
-          className="px-5 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors"
-        >
-          Enviar Intenção de Participação
-        </Link>
+        <div className="flex justify-center items-center flex-col gap-2">
+          <Link
+            href="/intencao"
+            className="px-5 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors"
+          >
+            Enviar Intenção de Participação
+          </Link>
+          <Link
+            href="/indicacao"
+            className="px-5 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors"
+          >
+            Sistema de Indicações
+          </Link>
+          <Link
+            href="/admin"
+            className="px-5 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition-colors"
+          >
+            Área do Administrador
+          </Link>
+        </div>
       </section>
     </div>
   );
